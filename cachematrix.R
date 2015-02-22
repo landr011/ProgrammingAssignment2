bd43f74edd6a8ba187486a2e38eb88db6aba4b6b
landr011 <luis.andradeg419@gmail.com>
2015-02-22 14:51:35 -0500

    Update cachematrix.R
    First function

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions to cache and compute the inverse matrix
+## Functionality is explained below:
 
-## Write a short comment describing this function
+## Start Funciton 1 to generate a special matrix which
+## can cache its own inverse
 
+## 1. Set values
 makeCacheMatrix <- function(x = matrix()) {
+                        set <- function (y) {
+                                x <<- y
+                                invmtx <<- NULL
+                                            }
+
+## 2. Get values
+        get <- function() return(x)
+
+## 2. Set the value of the inverse     
+        setinv <- function(inv) invmtx <<- inv
+
+## 3. Get the value of the inverse
+        getinv <- function() return(invmtx)
+        
+return(list(      set = set, 
+                  get = get,
+                  setinv  = setinv,
+                  getinv  = getinv)        
 
 }
 
